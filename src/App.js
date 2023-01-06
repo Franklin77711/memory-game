f489f28cb39d8a78b49e9132205a879f5078d3ca
Franklin77711 <robi1000350@gmail.com>
2023-01-06 15:40:38 +0100

    made the design and on click we save the clicked card's ID into state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, {useState} from "react";
 import style from "./style/App.css"
 import Cards from "./Components/Cards";
-import Score from "./Components/Score";
 
 const App = () => {
-  const [bestScore, setBestScore] = useState(0);
-  const [scoreRN, setScoreRN] = useState(0);
-
-
+  
   return (
     <div className="App">
       <h1>Dota 2 Memory Game</h1>
       <p>Get a point by clicking on a character which you didn't click already.</p>
-      <Score bestScore={bestScore} scoreRN={scoreRN}/>
-      <Cards />
+      <Cards/>
     </div>
   );
 }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {CardData} from "./Data/CardsData.js" 
+import style from "../style/Cards.css"
+import Score from "./Score.js";
 
 const Cards = () =>{
+    const [bestScore, setBestScore] = useState(0);
+    const [scoreRN, setScoreRN] = useState(0);
+    const [clickedCard, setClickedCard] =useState([]);
+
+    const CardClick = (card) =>{
+        setClickedCard(clickedCard.concat(card.id));
+    }
+
     return(
-         <div>
+         <div id="content">
+            <Score bestScore={bestScore} scoreRN={scoreRN}/>
             {CardData.map((data) => {
                 return(
-                    <div key={data.id} className="card">
+                    <div key={data.id} className="card" onClick={ () => CardClick(data)}>
                         <img src={data.img}></img>
                         <h2>{data.name}</h2>
                     </div>
                 )
             })}
         </div>
+        
     )
 }
 
